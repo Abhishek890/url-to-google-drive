21edf1edfda64490c6886dae6ec8713a748cf5c2
Abhishek890 <46078948+Abhishek890@users.noreply.github.com>
2020-01-21 13:38:22 +0530

    Update GoogleOauthController.java

--- src/main/java/com/github/dhaval_mehta/savetogoogledrive/controller/rest/oauth/GoogleOauthController.java
+++ src/main/java/com/github/dhaval_mehta/savetogoogledrive/controller/rest/oauth/GoogleOauthController.java
@@ -56,9 +56,9 @@ public class GoogleOauthController {
 	private final static String RESPONSE_TYPE = "code";
 	private final static String SCOPE = "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email";
 	private final static String GRANT_TYPE = "authorization_code";
-	private final static String CLIENT_ID = System.getenv(585442478292-bv54v44te51qdmqpdt189tqmtdhvd31j.apps.googleusercontent.com);
-	private final static String CLIENT_SECRET = System.getenv(rps_1Q3pYfKnz7Eb60KdrZu0);
-	private final static String REDIRECT_URI = System.getenv(https://urltodrivelinks.herokuapp.com);
+	private final static String CLIENT_ID = System.getenv("585442478292-bv54v44te51qdmqpdt189tqmtdhvd31j.apps.googleusercontent.com");
+	private final static String CLIENT_SECRET = System.getenv("rps_1Q3pYfKnz7Eb60KdrZu0");
+	private final static String REDIRECT_URI = System.getenv("https://urltodrivelinks.herokuapp.com");
 	private final static Gson gson = new GsonBuilder()
 			.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
 	private final HttpSession session;
